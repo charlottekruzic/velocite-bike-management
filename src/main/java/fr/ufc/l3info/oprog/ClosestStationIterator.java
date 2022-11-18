b42551ac274d651ac965ed43e252c0473b3b213f
charlottekruzic <charlotte.kruzic@outlook.com>
2022-11-18 15:33:04 +0100

    Def fonctions nouveaux fichiers

--- src/main/java/fr/ufc/l3info/oprog/ClosestStationIterator.java
+++ src/main/java/fr/ufc/l3info/oprog/ClosestStationIterator.java
@@ -1,4 +1,26 @@
 package fr.ufc.l3info.oprog;
 
+import java.util.Set;
+
 public class ClosestStationIterator {
+    public ClosestStationIterator(Set<Station> stations, Station s){
+
+    }
+
+    public boolean hasNext(){
+        return false;
+    }
+
+    public Station next(){
+        return null;
+    }
+
+    public void remove(){
+        return;
+    }
+
+
+
+
+
 }
